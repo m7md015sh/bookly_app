8f7e3e4f916cbf8cb8ef217dde0644e8f4f5c328
m7md015sh <m7md015shokry@gmail.com>
2024-03-18 17:33:39 +0200

    add google font package & and put montaguSlabTextTheme to main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/core/utils/colors/colors_constants.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Bookly());
@@ -14,8 +15,10 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  GetMaterialApp(
+
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: primaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: primaryColor,textTheme: GoogleFonts.montaguSlabTextTheme(ThemeData.dark().textTheme)),
+
       home: const SplashView(),
     );
   }
