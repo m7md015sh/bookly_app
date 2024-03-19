0bb3cbf0e26e60388ad0c6e551bc889a49eed8d0
m7md015sh <m7md015shokry@gmail.com>
2024-03-19 11:53:34 +0200

    create HomeDetails View

---
+++ lib/features/home/presentation/views/dooks_detalis_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/books_details_view_body.dart';
+
+class BooksDetailsView extends StatelessWidget {
+  const BooksDetailsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BooksDetailsViewBody(),
+    );
+  }
+}
