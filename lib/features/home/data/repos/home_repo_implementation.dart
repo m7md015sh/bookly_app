6cc8898a17041e97822429aa9e841ef83dd877fd
m7md015sh <m7md015shokry@gmail.com>
2024-03-21 04:37:44 +0200

    create api service

---
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImp extends HomeRepo{
+  @override
+  Future<Either<Failures, BookModel>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, BookModel>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+}
