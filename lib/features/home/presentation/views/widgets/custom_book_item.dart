4475abd3f9bcd47f32eab06194dde387850bbb43
m7md015sh <m7md015shokry@gmail.com>
2024-04-11 17:49:10 +0200

    Cached network image

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/assets/assets_paths.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,14 +10,9 @@ class CustomBookItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 3/4,
-      child: Container(
-
-        decoration:  BoxDecoration(
-
+      child: ClipRRect(
           borderRadius: BorderRadius.circular(16),
-          image:  DecorationImage(image: NetworkImage(imageUrl),fit: BoxFit.fill)
-        ),
-      ),
+          child: CachedNetworkImage(imageUrl: imageUrl,fit: BoxFit.cover,)),
     );
   }
 }
